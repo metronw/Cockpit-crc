f4ca2867b0a42bf7ea531c234756e761dd7ab3f7
Arthur Zanchet <admin@ind.com.py>
2025-01-17 17:09:20 -0300

    remove chamada de onOpenChange no bloco finally do AgentHeader

--- app/agent/components.tsx
+++ app/agent/components.tsx
@@ -71,9 +71,7 @@ export const AgentHeader = ({id}: {id?: number}) => {
       }
     } catch (error) {
       toast.error('Erro ao alterar o estado da interface.');
-    } finally {
-      onOpenChange(false);
-    }
+    } 
   };
 
   return (
